2eb8d61bc2f8bb1e330fbfd50994ee2cd722991e
jizhi <jizhi@DESKTOP-9TELPFT.lan>
2023-05-11 07:56:54 +0800

    配置修改

--- src/config/plugin/jizhi/webman-hashids/app.php
+++ src/config/plugin/jizhi/webman-hashids/app.php
@@ -2,7 +2,7 @@
 
 return [
     'enable' => true,
-    'salt' => getenv('APP_KEY'), //盐值，默认使用APP_KEY
-    'min_hash_length' => 0, //加密字符串的最小长度
-    'alphabet' => getenv('HASH_ID_ALPHABET', 'Fi1yqx4mk3Bda7DfMCjWoOSUHYTRKhuszl2cg5pXLe6AwEGn8NvJ9VtZr0IQbP')
+    'salt' => getenv('APP_KEY') ?: 'base64:dVdulDThU9cYbK7O5Y1cbENnYgToXBQ090GP05CkKZk=', //盐值，默认使用APP_KEY
+    'min_hash_length' => 8, //加密字符串的最小长度
+    'alphabet' => getenv('HASH_ID_ALPHABET') ?: 'Fi1yqx4mk3Bda7DfMCjWoOSUHYTRKhuszl2cg5pXLe6AwEGn8NvJ9VtZr0IQbP'
 ];
