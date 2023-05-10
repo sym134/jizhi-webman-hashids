1fa2d6793d732f5b7c4b5793dcc69e01efb801db
jizhi <jizhi@DESKTOP-9TELPFT.lan>
2023-05-11 07:39:19 +0800

    配置错误

--- src/config/plugin/jizhi/webman-hashids/app.php
+++ src/config/plugin/jizhi/webman-hashids/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'enable' => true,
     'salt' => getenv('APP_KEY'), //盐值，默认使用APP_KEY
     'min_hash_length' => 0, //加密字符串的最小长度
     'alphabet' => getenv('HASH_ID_ALPHABET', 'Fi1yqx4mk3Bda7DfMCjWoOSUHYTRKhuszl2cg5pXLe6AwEGn8NvJ9VtZr0IQbP')
--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 use Hashids\Hashids;
-use Jz\WebmanHashids\Exceptions\HashidException;
+use Jizhi\WebmanHashids\Exceptions\HashidException;
 
 /**
  * @param $int
@@ -14,7 +14,7 @@ function id_encode($int): string
     if (!is_numeric($int) || $int < 0 || !is_int($int + 0)) {
         throw new HashidException('Only positive integers can be accepted!');
     }
-    $hashid = new Hashids(config('plugin.jizhi.webman-hashids.salt'), config('plugin.jizhi.webman-hashids.min_hash_length'), config('plugin.jizhi.webman-hashids.alphabet'));
+    $hashid = new Hashids(config('plugin.jizhi.webman-hashids.app.salt'), config('plugin.jizhi.webman-hashids.app.min_hash_length'), config('plugin.jizhi.webman-hashids.app.alphabet'));
     return $hashid->encode($int);
 }
 
@@ -29,7 +29,7 @@ function id_decode($str)
     if (!preg_match('/^[0-9a-zA-Z]{2,18}$/', $str)) {
         throw new HashidException('Bad parameter! Between 2-18 characters');
     }
-    $hashid = new Hashids(config('plugin.jizhi.webman-hashids.salt'), config('plugin.jizhi.webman-hashids.min_hash_length'), config('plugin.jizhi.webman-hashids.alphabet'));
+    $hashid = new Hashids(config('plugin.jizhi.webman-hashids.app.salt'), config('plugin.jizhi.webman-hashids.app.min_hash_length'), config('plugin.jizhi.webman-hashids.app.alphabet'));
     $result = $hashid->decode($str);
     if (count($result) !== 1) {
         throw new HashidException('Unable to decrypt!');
